4614e423e99354783feb0268f695db83fb21aada
Claudia Kosylak <claudiakosylak@gmail.com>
2025-01-27 13:16:22 +0100

    try diff aspect ratio

--- src/components/Scanner/ScannerOverlay.tsx
+++ src/components/Scanner/ScannerOverlay.tsx
@@ -17,6 +17,7 @@ export const ScannerOverlay: React.FC<ScannerOverlayProps> = ({ onScan }) => {
             }
           }}
           delay={300}
+          videoConstraints={{aspectRatio: 2}}
         />
       </div>
       <div className="absolute inset-0 pointer-events-none">
