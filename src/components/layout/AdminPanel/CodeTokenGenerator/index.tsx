2510bfc75d4a8176e510b0b8843137abcc20be7e
Claudia Kosylak <claudiakosylak@gmail.com>
2025-02-06 15:15:36 +0100

    minor styling fix

--- src/components/layout/AdminPanel/CodeTokenGenerator/index.tsx
+++ src/components/layout/AdminPanel/CodeTokenGenerator/index.tsx
@@ -38,120 +38,117 @@ export const CodeTokenGenerator: React.FC = () => {
           <p className="text-gray-600 mt-1">Create in bulk maybe</p>
         </div>
       </div>
-      <div className="max-w-4xl mx-auto p-4">
-        <div className="mb-8 p-6 bg-white rounded-lg shadow-lg">
-          <div
-            className={!isMobile ? "grid grid-cols-2 gap-4" : undefined}
-            style={
-              isMobile
-                ? { display: "flex", flexDirection: "column", gap: 20 }
-                : undefined
-            }
-          >
-            <FormInput
-              label="Quantity of codes to generate"
-              type="number"
-              value={bulkQuantity.toString()}
-              required
-              onChange={(val: string) => setBulkQuantity(parseInt(val))}
-            />
-            <TokenSettingsChecks
-              tokenSettings={tokenSettings}
-              setTokenSettings={setTokenSettings}
-            />
-            <div className="mt-4 col-span-2">
-              <label className="block mb-2 text-sm font-medium text-gray-700">
-                Length: {tokenSettings.length}
-              </label>
-              <input
-                type="range"
-                min="1"
-                max="512"
-                step="1"
-                value={tokenSettings.length}
-                onChange={(e) =>
-                  setTokenSettings({
-                    ...tokenSettings,
-                    length: Number(e.target.value),
-                  })
-                }
-                className="w-full"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                Foreground Color:
-              </label>
-              <input
-                type="color"
-                value={qrCodeSettings.foreground}
-                onChange={(e) =>
-                  setQrCodeSettings({
-                    ...qrCodeSettings,
-                    foreground: e.target.value,
-                  })
-                }
-                className="w-full h-10"
-              />
-            </div>
-
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                Background Color:
-              </label>
-              <input
-                type="color"
-                value={qrCodeSettings.background}
-                onChange={(e) =>
-                  setQrCodeSettings({
-                    ...qrCodeSettings,
-                    background: e.target.value,
-                  })
-                }
-                className="w-full h-10"
-              />
-            </div>
-
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                Error Resistance:
-              </label>
-              <select
-                value={qrCodeSettings.errorCorrectionLevel}
-                onChange={(e) =>
-                  setQrCodeSettings({
-                    ...qrCodeSettings,
-                    errorCorrectionLevel: e.target
-                      .value as ErrorCorrectionLevel,
-                  })
-                }
-                className="w-full p-2 border rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-              >
-                {errorCorrectionLevels.map((level) => (
-                  <option key={level} value={level}>
-                    {level}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </div>
-          <div className="flex w-full mt-6 justify-end">
-            <button
-              type="button"
-              onClick={() =>
-                generateAndDownloadZip(
-                  bulkQuantity,
-                  tokenSettings,
-                  qrCodeSettings
-                )
+      <div className="mb-8 p-6 bg-white rounded-lg shadow-lg">
+        <div
+          className={!isMobile ? "grid grid-cols-2 gap-4" : undefined}
+          style={
+            isMobile
+              ? { display: "flex", flexDirection: "column", gap: 20 }
+              : undefined
+          }
+        >
+          <FormInput
+            label="Quantity of codes to generate"
+            type="number"
+            value={bulkQuantity.toString()}
+            required
+            onChange={(val: string) => setBulkQuantity(parseInt(val))}
+          />
+          <TokenSettingsChecks
+            tokenSettings={tokenSettings}
+            setTokenSettings={setTokenSettings}
+          />
+          <div className="mt-4 col-span-2">
+            <label className="block mb-2 text-sm font-medium text-gray-700">
+              Length: {tokenSettings.length}
+            </label>
+            <input
+              type="range"
+              min="1"
+              max="512"
+              step="1"
+              value={tokenSettings.length}
+              onChange={(e) =>
+                setTokenSettings({
+                  ...tokenSettings,
+                  length: Number(e.target.value),
+                })
               }
-              className={`px-4 py-${
-                isMobile ? 3 : 2
-              } bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors`}
-            >
-              Generate and Download Zip
-            </button>
+              className="w-full"
+            />
           </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700">
+              Foreground Color:
+            </label>
+            <input
+              type="color"
+              value={qrCodeSettings.foreground}
+              onChange={(e) =>
+                setQrCodeSettings({
+                  ...qrCodeSettings,
+                  foreground: e.target.value,
+                })
+              }
+              className="w-full h-10"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700">
+              Background Color:
+            </label>
+            <input
+              type="color"
+              value={qrCodeSettings.background}
+              onChange={(e) =>
+                setQrCodeSettings({
+                  ...qrCodeSettings,
+                  background: e.target.value,
+                })
+              }
+              className="w-full h-10"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700">
+              Error Resistance:
+            </label>
+            <select
+              value={qrCodeSettings.errorCorrectionLevel}
+              onChange={(e) =>
+                setQrCodeSettings({
+                  ...qrCodeSettings,
+                  errorCorrectionLevel: e.target.value as ErrorCorrectionLevel,
+                })
+              }
+              className="w-full p-2 border rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            >
+              {errorCorrectionLevels.map((level) => (
+                <option key={level} value={level}>
+                  {level}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div className="flex w-full mt-6 justify-end">
+          <button
+            type="button"
+            onClick={() =>
+              generateAndDownloadZip(
+                bulkQuantity,
+                tokenSettings,
+                qrCodeSettings
+              )
+            }
+            className={`px-4 py-${
+              isMobile ? 3 : 2
+            } bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors`}
+          >
+            Generate and Download Zip
+          </button>
         </div>
       </div>
     </div>
